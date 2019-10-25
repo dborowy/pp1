694fd46f16d837621ac0c9db9ebe12a82b5a7a77
Dominik Borowy <dominikborowy@gmail.com>
2019-10-25 23:59:57 +0200

    Praca w domu

---
+++ 02-ControlStructures/02_afterClass.py
@@ -0,0 +1,42 @@
+############ 25
+#for i in range(3):
+#    print(7*'*')
+    
+############ 26
+#for i in range(1,10):
+#    print(i*f'{i}')
+
+############ 27
+#i=1
+#while i<5:
+#    print(i*'* ')
+#    i+=1
+#while i>0:
+#    print(i*'* ')
+#    i-=1
+
+############ 28
+#a = 4
+#b = 15
+#print(b*'*'+'\n'+(a-2)*('*'+(b-2)*' '+'*'+'\n')+b*'*')
+
+############ 29
+#lista = [15, 8, 31, 47, 2, 19]
+#print(lista[::-1])
+
+############ 30
+#i = 3
+#pinPop = '0805'
+#while i>=0:
+#    if i == 0:
+#        print('Karta płatnicza zostaje zablokowana')
+#        break
+#    pinWp = input('Podaj kod PIN: ')
+#    if pinWp == pinPop:
+#        print('PIN poprawny')
+#        break
+#    else:
+#        print('PIN niepoprawny')
+#        i-=1
+
+############ 31
