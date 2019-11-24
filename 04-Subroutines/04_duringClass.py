7a7abbae6cd9418ba12c91845cf70ba8f8b77fb8
Dominik Borowy <dominikborowy@gmail.com>
2019-11-24 20:24:37 +0100

    praca w domu

--- 04-Subroutines/04_duringClass.py
+++ 04-Subroutines/04_duringClass.py
@@ -101,3 +101,90 @@
 #print(f'5! = {silnia(5)}')
 
 ########### 19
+#def suma(N):
+#    if N==1:
+#        return 1
+#    if N>0:
+#        return N + suma(N-1)
+#print(suma(500))
+
+########### 20
+#def potega(x,n):
+#    if n>1:
+#        return x * potega(x,n-1)
+#    else:
+#        return x
+#print(potega(2,10))
+
+########### 22
+#even = lambda x: x%2==0
+#print(even(5))
+
+########### 23
+#bigger = lambda x,y: x>y
+#print(bigger(3,2))
+
+########### 24
+#def miesiac(x):
+#    listaMiesiecy = ['sty','lut','marz','kwie','maj','czerw',]
+#    return listaMiesiecy[x-1]
+#print(miesiac(2))
+
+########### 25
+#def imieIn(x):
+#    imiona = ['Janek', 'Ania', 'Wojtek', 'Zosia']
+#    imionaStr = ' '.join(imiona)
+#    if x in imiona:
+#        wynik = ''
+#    else:
+#        wynik = 'nie '
+#    print(f'Imiona: {imionaStr}\nImie: {x}\nRezultat: imię {wynik}zawarte jest w wykazie imion')
+#imieIn('Janek')
+
+########### 26
+#def podatek(dochod):
+#    dochod = int(dochod)
+#    if dochod<=5000:
+#        tax = int(dochod*0.17)
+#    else:
+#        tax = int(5000*0.17 + (dochod-5000)*0.32)
+#    print(f'Podany dochod: {dochod}\nPodatek nalezny: {tax} zł')
+#podatek(6000)
+
+########### 27
+#def ileSam():
+#    import re
+#    text = 'Nam strzelać nie kazano. Wstąpiłem na działo. I spojrzałem na pole, dwieście armat grzmiało. Artyleryji ruskiej ciągną się szeregi, Prosto, długo, daleko, jako morza brzegi.'
+#    listaSam = re.findall('a|e|i|o|u|y|ą|ę|ó',text)
+#    return len(listaSam)
+#print(ileSam())
+
+########### 28
+#def langGraph():
+#    lang = ['Java','Python','JavaScript','C++','C#','Ruby','Perl','PHP','C','Android']
+#    demand = [61,47,37,32,26,18,14,14,9,7]
+#    langLineup = []
+#    longestLang = len(max(lang,key=len))
+#    for i in lang:
+#        langLineup.append((' '*(longestLang-len(i))+i))
+#        
+#    for i in range(len(lang)-1):
+#        hasht = '#'*demand[i]
+#        print(f'{langLineup[i]}: {hasht}')
+#langGraph()
+
+########### 29
+#tab = [2, 3, 5, 2, 9, 8, 1, 3, 9, 1, 1, 4, 7, 7, 1, 4]
+#def medAndDom(tablica):
+#    tablica.sort
+#    if len(tablica)%2==0
+
+########### 32
+def transp(x):
+    xTransp = []
+    for i in range(len(x)-1):
+        xTransp.append(x[i][:len(x[0])-1])
+    
+    return xTransp
+mac = [[1,2,0],[0,0,3],[5,1,1]]
+print(transp(mac))
